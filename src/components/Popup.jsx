2ac5b3e6d56e4428da9a5e33c195e70e9253d782
DCABHI83 <araj10569@gmail.com>
2026-03-21 12:56:25 +0530

    added some more gitignore and made popup page

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Popup = () => {
+  const [form,setForm] = useState({
+    id:"",
+    name:"",
+    category:"",
+    stock:"",
+    price:"",
+    status:""
+
+  })
+  const handleSubmit = ()=>{
+
+  }
   return (
     <>
     <div>
         <form >
            <div>
              <label htmlFor="id">id</label>
-            <input type="text" placeholder='Enter Id' />
+            <input type="text" name='id' value={form.id} onChange={(e)=>setForm(e.target.value)} placeholder='Enter Id' />
             <label htmlFor="name">name</label>
-            <input type="text" placeholder='Enter the name of the product' />
+            <input type="text" name='name' value={form.name} onChange={(e)=>setForm(e.target.value)} placeholder='Enter the name of the product' />
             <label htmlFor="category">Category</label>
-            <input type="text" placeholder='Enter the Category' />
+            <input type="text" name='category' value={form.category} onChange={(e)=>setForm(e.target.value)} placeholder='Enter the Category' />
             <label htmlFor="stock">Stock</label>
-            <input type="text" placeholder='Enter Stock' />
+            <input type="text" name='stock' value={form.stock} onChange={(e)=>setForm(e.target.value)} placeholder='Enter Stock' />
             <label htmlFor="price">Price</label>
-            <input type="text" placeholder='Enter Price' />
+            <input type="text" name='price' value={form.price} onChange={(e)=>setForm(e.target.value)} placeholder='Enter Price' />
             <label htmlFor="status">Status</label>
-            <input type="text" placeholder='Enter Status' />
+            <input type="text" name='status' value={form.status} onChange={(e)=>setForm(e.target.value)} placeholder='Enter Status' />
            </div>
            <button onClick={handleSubmit
             
